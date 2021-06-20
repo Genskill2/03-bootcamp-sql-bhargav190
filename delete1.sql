3e7297ea734fde78de56ff145dd6ff709a2e4510
hungry-one <bharath_b180953cs@nitc.ac.in>
2021-06-20 11:47:59 +0530

    answers

---
+++ delete1.sql
@@ -0,0 +1,6 @@
+DELETE FROM books_subjects WHERE subject IN (
+        SELECT id FROM subjects WHERE name = 'History'
+);
+
+DELETE FROM subjects WHERE name = 'History';
+
---
+++ query1.sql
@@ -0,0 +1,4 @@
+SELECT title FROM books WHERE publisher IN (
+        SELECT id FROM publisher WHERE name = 'PHI'
+);
+
